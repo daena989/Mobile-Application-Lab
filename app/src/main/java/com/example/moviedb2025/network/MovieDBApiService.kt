f6a096484b099a7c1f7026a611eef2193945a203
daena989 <144349940+daena989@users.noreply.github.com>
2025-04-24 16:06:17 +0200

    updated files added from class live coding

--- app/src/main/java/com/example/moviedb2025/network/MovieDBApiService.kt
+++ app/src/main/java/com/example/moviedb2025/network/MovieDBApiService.kt
@@ -1,4 +1,21 @@
-package com.example.moviedb2025.network;
+package com.example.moviedb2025.network
 
-public interface MovieDBApiService {
+//Should have more lines of code here but cannot see in photos
+import com.example.moviedb2025.models.MovieResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieDBApiService {
+
+    @GET("popular")
+    suspend fun getPopularMovies(
+        @Query("api_key")
+        apikey: String = Constants.API.KEY
+    ): MovieResponse
+
+    @GET("top-rated")
+    suspend fun getTopRatedMovies(
+        @Query("api_key")
+        apikey: String = Constants.API.KEY
+    ): MovieResponse
 }
